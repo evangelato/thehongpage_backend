4288e8a74e4d325d95a29bf34f1b1947582904ad
Evan Hong <evangelurtz@gmail.com>
2020-02-19 16:04:12 +0900

    First commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
+
+module.exports = server;
