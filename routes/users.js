b2529a7055d7e676fe2a8d2277f56df4e08fcbbb
Evan Hong <evangelurtz@gmail.com>
2020-02-20 20:09:20 +0900

    Fix object id validation code

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,29 @@
 const auth = require('../middleware/auth');
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
-const {User, validate} = require('../models/user');
+const { User, validate } = require('../models/user');
 const express = require('express');
 const router = express.Router();
 
 router.get('/me', auth, async (req, res) => {
-  const user = await User.findById(req.user._id).select('-password');
-  res.send(user);
+    const user = await User.findById(req.user._id).select('-password');
+    res.send(user);
 });
 
 router.post('/', async (req, res) => {
-  const { error } = validate(req.body); 
-  if (error) return res.status(400).send(error.details[0].message);
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
-  let user = await User.findOne({ username: req.body.username });
-  if (user) return res.status(400).send('This username is in use.');
+    let user = await User.findOne({ username: req.body.username });
+    if (user) return res.status(400).send('This username is in use.');
 
-  user = new User(_.pick(req.body, ['name', 'username', 'password']));
-  const salt = await bcrypt.genSalt(10);
-  user.password = await bcrypt.hash(user.password, salt);
-  await user.save();
+    user = new User(_.pick(req.body, ['name', 'username', 'password']));
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    await user.save();
 
-  const token = user.generateAuthToken();
-  res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'username']));
+    const token = user.generateAuthToken();
+    res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'username']));
 });
 
-module.exports = router; 
+module.exports = router;
