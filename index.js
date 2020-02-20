8aff13f04ac01e30929e9baa9ba0b527212dca51
Evan Hong <evangelurtz@gmail.com>
2020-02-20 16:13:30 +0900

    Add logging with winston

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const winston = require('winston');
 const express = require('express');
 const app = express();
 
-require('./startup/db')();
+require('./startup/logging')();
 require('./startup/routes')(app);
+require('./startup/db')();
+// require('./startup/config')();
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
+const server = app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+    winston.info(`Listening on port ${port}...`);
+});
 
 module.exports = server;
